0537c4e9af80646374bf2f229871c0f37403d40d
Graeme Erickson <graemeerickson@gmail.com>
2019-02-01 10:34:25 -0800

    Boolean type practice

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+const pizzas = 2;
+function offerDiscount(orders) {
+    return orders >= 3;
+}
+if (offerDiscount(pizzas)) {
+    console.log(`You're entitled to a discount!`);
+}
+else {
+    console.log(`Order more than 3 pizzas for a discount!`);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello TypeScript!');
+const pizzas: number = 2;
+
+function offerDiscount(orders: number): boolean {
+  return orders >= 3;
+}
+
+if (offerDiscount(pizzas)) {
+  console.log(`You're entitled to a discount!`);
+} else {
+  console.log(`Order more than 3 pizzas for a discount!`);
+}
