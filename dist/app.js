1fcffc3d6ba4c21b8bec223c0a91f1cd5234e486
Graeme Erickson <graemeerickson@gmail.com>
2019-02-01 11:55:17 -0800

    Practice with array generic types

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,6 @@
 "use strict";
 const sizes = ['small', 'medium', 'large'];
 console.log(`Sizes: ${sizes}`);
+let toppings;
+toppings = ['pepperoni', 'tomato', 'bacon'];
+console.log(`Toppings: ${toppings}`);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
 const sizes: string[] = ['small', 'medium', 'large'];
-
 console.log(`Sizes: ${sizes}`);
+
+let toppings: Array<string>;
+toppings = ['pepperoni', 'tomato', 'bacon'];
+console.log(`Toppings: ${toppings}`);
